d8a69d85448bd6be9ecc8ffd9ec2e0e9960036c0
sandeep-sanjuu <sandeepsanjuu3@gmail.com>
2024-06-10 21:03:42 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFullStackApplication.class, args);
+	}
+
+}
